f7f38dfef281a38aa5b85ae8d47632a5d0dc2220
NerdSmith <merzlyakov02@gmail.com>
2022-06-08 15:41:30 +0300

    added ping_receive_one

--- ping.cpp
+++ ping.cpp
@@ -6,154 +6,140 @@
 #include <netinet/ip.h>
 
 void printBytes(PingPkgRecv &recvPkg) {
-  for (int i = 0; i < sizeof(recvPkg); i++) {
-    std::cout << std::hex << (int)((char *)&recvPkg)[i] << " ";
-  }
-  std::cout << std::endl;
+    for (int i = 0; i < sizeof(recvPkg); i++) {
+        std::cout << std::hex << (int)((char *)&recvPkg)[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 void printBytes(PingPkg &pkg) {
-  for (int i = 0; i < sizeof(pkg); i++) {
-    std::cout << std::hex << (int)((char *)&pkg)[i] << " ";
-  }
-  std::cout << std::endl;
+    for (int i = 0; i < sizeof(pkg); i++) {
+        std::cout << std::hex << (int)((char *)&pkg)[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int ping_receive_one(int sock, std::map<std::uint32_t, PingConfig> &addrCfgs) {
+    struct msghdr msg_hdr;
+    char payload_buffer[4096];
+    ssize_t payload_buffer_len;
+    char control_buffer[4096];
+    struct iovec payload_iovec;
+
+    int data_offset;
+    uint32_t msg_key;
+
+    memset(&payload_iovec, 0, sizeof(payload_iovec));
+    payload_iovec.iov_base = payload_buffer;
+    payload_iovec.iov_len = sizeof(payload_buffer);
+
+    memset(&msg_hdr, 0, sizeof(msg_hdr));
+
+    msg_hdr.msg_name = NULL;
+    msg_hdr.msg_namelen = 0;
+
+    msg_hdr.msg_iov = &payload_iovec;
+    msg_hdr.msg_iovlen = 1;
+
+    msg_hdr.msg_control = control_buffer;
+    msg_hdr.msg_controllen = sizeof(control_buffer);
+
+    msg_hdr.msg_flags = 0;
+
+    payload_buffer_len = recvmsg(sock, &msg_hdr, 0);
+
+    if (payload_buffer_len < 0) {
+        return -1;
+    }
+
+    data_offset = payload_buffer_len - PING_PKT_S + sizeof(icmphdr);
+
+    msg_key = *((uint32_t *)(payload_buffer + (payload_buffer_len - PING_PKT_S +
+                                               sizeof(icmphdr))));
+    if (addrCfgs.find(msg_key) != addrCfgs.end()) {
+        addrCfgs[msg_key].status = PingStatus::OK;
+    }
+
+    return 0;
 }
 
 unsigned short checksum(void *b, int len) {
-  unsigned short *buf = (unsigned short *)b;
-  unsigned int sum = 0;
-  unsigned short result;
+    unsigned short *buf = (unsigned short *)b;
+    unsigned int sum = 0;
+    unsigned short result;
 
-  for (sum = 0; len > 1; len -= 2)
-    sum += *buf++;
-  if (len == 1)
-    sum += *(unsigned char *)buf;
-  sum = (sum >> 16) + (sum & 0xFFFF);
-  sum += (sum >> 16);
-  result = ~sum;
-  return result;
+    for (sum = 0; len > 1; len -= 2)
+        sum += *buf++;
+    if (len == 1)
+        sum += *(unsigned char *)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
 }
 
 Ping::Ping(std::map<std::uint32_t, PingConfig> addrCfgs) {
-  this->addrCfgs = addrCfgs;
+    this->addrCfgs = addrCfgs;
 }
 
 bool Ping::Init() {
-  for (const auto &kv : this->addrCfgs) {
+    for (const auto &kv : this->addrCfgs) {
 
-    addrToSockAddr[kv.second.IP]; //.insert(std::pair<std::string,
-                                  // sockaddr_in>(kv.second.IP, sockaddr_in()));
-    addrToSockAddr[kv.second.IP].sin_family = AF_INET;
-    addrToSockAddr[kv.second.IP].sin_addr.s_addr =
-        inet_addr(kv.second.IP.c_str());
-
-    if ((addrToSock[kv.second.IP] = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) <
-        0) {
-      return false;
+        addrToSockAddr[kv.second.IP]; //.insert(std::pair<std::string,
+                                      // sockaddr_in>(kv.second.IP,
+                                      // sockaddr_in()));
+        addrToSockAddr[kv.second.IP].sin_family = AF_INET;
+        addrToSockAddr[kv.second.IP].sin_addr.s_addr =
+            inet_addr(kv.second.IP.c_str());
     }
-  }
+    if ((sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
 std::map<std::uint32_t, PingConfig> Ping::Exec() {
-  PingPkg pkg;
-  bzero(&pkg, sizeof(pkg));
-  pkg.hdr.type = ICMP_ECHO;
-  pkg.hdr.un.echo.id = getpid() % std::numeric_limits<u_int16_t>::max();
+    int max_fd = sock;
+    timeval timeout{0, 1000000};
 
-  PingPkgRecv recvPkg;
-  bzero(&recvPkg, sizeof(recvPkg));
+    for (std::map<std::uint32_t, PingConfig>::iterator it =
+             this->addrCfgs.begin();
+         it != this->addrCfgs.end(); ++it) {
 
-  int fromlen, selectRes;
+        fd_set read_fds;
+        fd_set write_fds;
 
-  std::string currIP;
-  timeval timeout{0, 100000};
+        int max_fd = -1;
 
-  // 4 sock read & write
-  fd_set toRead, toSend;
-  int maxSockNb = -1;
-  FD_ZERO(&toRead);
-  FD_ZERO(&toSend);
-  for (std::map<std::uint32_t, PingConfig>::iterator it =
-           this->addrCfgs.begin();
-       it != this->addrCfgs.end(); ++it) {
-    currIP = it->second.IP;
-    if (addrToSock[currIP] > maxSockNb) {
-      maxSockNb = addrToSock[currIP];
+        FD_ZERO(&read_fds);
+        FD_ZERO(&write_fds);
+
+        if (it->second.status == PingStatus::W_4_ANSV) {
+            FD_SET(sock, &read_fds);
+        }
+
+        if (it->second.status == PingStatus::W_4_SEND) {
+            FD_SET(sock, &write_fds);
+        }
+
+        int status = select(max_fd + 1, &read_fds, &write_fds, NULL, &timeout);
+
+        if (status == -1) {
+            std::cout << "ERR" << std::endl;
+            return this->addrCfgs;
+        } else if (status == 0) {
+            std::cout << "timeout" << std::endl;
+            continue;
+        }
+
+        if (FD_ISSET(sock, &read_fds)) {
+            ping_receive_one(this->sock, this->addrCfgs);
+            continue;
+        }
     }
 
-    if (it->second.status == PingStatus::W_4_ANSV) {
-      FD_SET(addrToSock[currIP], &toRead);
-    } else if (it->second.status == PingStatus::W_4_SEND) {
-      FD_SET(addrToSock[currIP], &toSend);
-    }
-  }
-
-  // select block
-  selectRes = select(maxSockNb + 1, &toRead, &toSend, NULL, &timeout);
-
-  // if (selectRes == 0) {
-  //     std::cout << "timeout" << std::endl;
-  //     for(std::map<std::uint32_t, PingConfig>::iterator it =
-  //     this->addrCfgs.begin(); it != this->addrCfgs.end(); ++it) {
-  //         currIP = it->second.IP;
-  //         if (it->second.status == PingStatus::W_4_ANSV) {
-  //             it->second.status = PingStatus::W_4_SEND;
-  //             FD_SET(addrToSock[currIP], &toSend);
-  //         }
-  //         // 4 err
-  //         // else if (it->second.status == PingStatus::ERR) {
-  //         //     FD_SET(addrToSock[currIP], &toSend);
-  //         // }
-  //     }
-  // }
-
-  for (std::map<std::uint32_t, PingConfig>::iterator it =
-           this->addrCfgs.begin();
-       it != this->addrCfgs.end(); ++it) {
-    currIP = it->second.IP;
-
-    // read
-    if (FD_ISSET(addrToSock[currIP], &toRead)) {
-      std::cout << "read" << std::endl;
-      bzero(&recvPkg, sizeof(recvPkg));
-      fromlen = sizeof(sockaddr_in);
-
-      if (recvfrom(addrToSock[currIP], &recvPkg, sizeof(recvPkg), 0,
-                   (struct sockaddr *)&(addrToSockAddr[currIP]),
-                   (socklen_t *)&fromlen) <= 0) {
-        it->second.status = PingStatus::ERR;
-      } else {
-        printBytes(recvPkg);
-        // std::cout << "ansv: " << *((uint32_t*) recvPkg.ping_pkg.msg) <<
-        // std::endl;
-        this->addrCfgs[*((uint32_t *)recvPkg.ping_pkg.msg)].status =
-            PingStatus::OK;
-      }
-    }
-
-    // write
-    else if (FD_ISSET(addrToSock[currIP], &toSend)) {
-      pkg.setNbAsMsg(it->first);
-
-      pkg.hdr.un.echo.sequence = this->seqCounter.nb;
-      this->seqCounter++;
-
-      pkg.hdr.checksum = checksum(&pkg, sizeof(pkg));
-
-      if (sendto(addrToSock[currIP], &pkg, sizeof(pkg), 0,
-                 (struct sockaddr *)&(this->addrToSockAddr[currIP]),
-                 (socklen_t)sizeof(sockaddr_in)) <= 0) {
-        it->second.status = PingStatus::ERR;
-        printf("\nPacket Sending Failed!\n");
-      } else {
-        it->second.status = PingStatus::W_4_ANSV;
-      }
-    }
-  }
-
-  return this->addrCfgs;
+    return this->addrCfgs;
 }
 
 Ping::~Ping() {}
--- ping.h
+++ ping.h
@@ -22,36 +22,36 @@
 // #define TIMEOUT timeval {1, 0}
 
 struct SeqCounter {
-  u_int16_t nb = 0;
+    u_int16_t nb = 0;
 
-  SeqCounter &operator++(int) {
-    if (this->nb == std::numeric_limits<u_int16_t>::max()) {
-      this->nb = 0;
-    } else {
-      this->nb++;
+    SeqCounter &operator++(int) {
+        if (this->nb == std::numeric_limits<u_int16_t>::max()) {
+            this->nb = 0;
+        } else {
+            this->nb++;
+        }
+        return *this;
     }
-    return *this;
-  }
 };
 
 #pragma pack(push, 1)
 struct PingPkg {
-  struct icmphdr hdr;
-  char msg[PING_PKT_S - sizeof(icmphdr)];
+    struct icmphdr hdr;
+    char msg[PING_PKT_S - sizeof(icmphdr)];
 
-  void setNbAsMsg(uint32_t nb) {
-    bzero(&(this->msg), sizeof(this->msg));
-    uint32_t *uint32_msg = (uint32_t *)(this->msg);
-    *uint32_msg = nb;
-    return;
-  }
+    void setNbAsMsg(uint32_t nb) {
+        bzero(&(this->msg), sizeof(this->msg));
+        uint32_t *uint32_msg = (uint32_t *)(this->msg);
+        *uint32_msg = nb;
+        return;
+    }
 };
 #pragma pack(pop)
 
 #pragma pack(push, 1)
 struct PingPkgRecv {
-  struct ip ip_hdr;
-  struct PingPkg ping_pkg;
+    struct ip ip_hdr;
+    struct PingPkg ping_pkg;
 };
 #pragma pack(pop)
 
@@ -60,26 +60,27 @@ unsigned short checksum(void *b, int len);
 enum class PingStatus { W_4_SEND, W_4_ANSV, OK, ERR };
 
 struct PingConfig {
-  std::string IP;
-  //    std::uint32_t port;
-  std::string name;
-  std::uint32_t Id;
-  PingStatus status = PingStatus::W_4_SEND;
+    std::string IP;
+    //    std::uint32_t port;
+    std::string name;
+    std::uint32_t Id;
+    PingStatus status = PingStatus::W_4_SEND;
 };
 
 class Ping {
-private:
-  std::map<std::uint32_t, PingConfig> addrCfgs;
-  std::map<std::string, sockaddr_in> addrToSockAddr;
-  std::map<std::string, int> addrToSock;
-  SeqCounter seqCounter;
-  // PingPkg pingPkg;
+  private:
+    std::map<std::uint32_t, PingConfig> addrCfgs;
+    std::map<std::string, sockaddr_in> addrToSockAddr;
+    int sock;
+    // std::map<std::string, int> addrToSock;
+    SeqCounter seqCounter;
+    // PingPkg pingPkg;
 
-public:
-  Ping(std::map<std::uint32_t, PingConfig> addrCfgs);
-  bool Init();
-  std::map<std::uint32_t, PingConfig> Exec();
-  ~Ping();
+  public:
+    Ping(std::map<std::uint32_t, PingConfig> addrCfgs);
+    bool Init();
+    std::map<std::uint32_t, PingConfig> Exec();
+    ~Ping();
 };
 
 #endif // PING_H
